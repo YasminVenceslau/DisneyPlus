db850bb71a273ee75871e3fd0d2d6372f5264209
YasminVenceslau <yasmin.venceslau@hotmail.com>
2024-09-13 00:53:53 -0300

    conf uglify

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,15 @@ const sass = require('gulp-sass')(require('sass'));
 // Importa o módulo 'gulp-imagemin' para otimizar imagens.
 const imagemin = require('gulp-imagemin');
 
+const uglify = require('gulp-uglify');
+
+
+function scripts(){
+    return gulp.src('./src/scripts/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/js'))
+}
+
 // Função responsável por processar arquivos SCSS.
 function styles() {
     return gulp.src('./src/styles/*.scss') // Seleciona todos os arquivos SCSS na pasta 'src/styles'.
@@ -22,9 +31,10 @@ function images() {
 }
 
 // Tarefa padrão que executa as funções de processamento de estilos e imagens em paralelo.
-exports.default = gulp.parallel(styles, images);
+exports.default = gulp.parallel(styles, images, scripts);
 
 // Função que observa mudanças nos arquivos SCSS e reexecuta a função 'styles' automaticamente.
 exports.watch = function(){
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles)) // Observa a pasta 'src/styles' por mudanças nos arquivos SCSS.
+    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts))
 }
